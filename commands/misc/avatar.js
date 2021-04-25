62c3517d5844020497804de2efadba56854d895a
InfoBlock <36295870+InfoBlock@users.noreply.github.com>
2021-04-26 00:09:31 +0200

    Update avatar.js

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -16,17 +16,17 @@ module.exports.execute = async(bot, msg, args) {
    let avEmbed = new Discord.MessageEmbed();
    
    if (!message.mentions.users.first()) {
-     avEmbed.setTitle('Your Avatar!');
+     avEmbed.setTitle('Your avatar!');
      avEmbed.setColor('RANDOM');
-     avEmbed.setThumbnail(message.author.displayAvatarURL());
+     avEmbed.setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
      
      return message.channel.send(avEmbed);
    } else {
      let target = message.mentions.members.first();
      
-     avEmbed.setTitle(`${bot.users.cache.get(target.id).tag}'s Avatar!`);
+     avEmbed.setTitle(`${bot.users.cache.get(target.id).tag}'s avatar!`);
      avEmbed.setColor('RANDOM');
-     avEmbed.setThumbnail(bot.users.cache.get(target.id).displayAvatarURL());
+     avEmbed.setThumbnail(bot.users.cache.get(target.id).displayAvatarURL({ dynamic: true }));
      
      return message.channel.send(avEmbed);
 }
