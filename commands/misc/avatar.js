556a5e2fa6ff92d876bd3607a8e0b4305e30967d
InfoBlock <36295870+InfoBlock@users.noreply.github.com>
2021-04-26 19:51:00 +0200

    Update & (hopefully) fix avatar.js
    Avatar now below Text and shown before "<User>'s avatar"
    Also when no one pinged your avatar should be shown now.

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -16,17 +16,17 @@ module.exports.execute = async(bot, msg, args) {
    let avEmbed = new Discord.MessageEmbed();
    
    if (!message.mentions.users.first()) {
-     avEmbed.setTitle('Your avatar!');
+     avEmbed.setAuthor(`${bot.users.cache.get(target.id).tag}'s avatar!`, message.author.displayAvatarURL());
      avEmbed.setColor('RANDOM');
-     avEmbed.setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
+     avEmbed.setImage(message.author.displayAvatarURL({ dynamic: true }));
      
      return message.channel.send(avEmbed);
    } else {
      let target = message.mentions.members.first();
      
-     avEmbed.setTitle(`${bot.users.cache.get(target.id).tag}'s avatar!`);
+     avEmbed.setTitle(`${bot.users.cache.get(target.id).tag}'s avatar!`, target.user.displayAvatarURL());
      avEmbed.setColor('RANDOM');
-     avEmbed.setThumbnail(bot.users.cache.get(target.id).displayAvatarURL({ dynamic: true }));
+     avEmbed.setImage(bot.users.cache.get(target.id).displayAvatarURL({ dynamic: true }));
      
      return message.channel.send(avEmbed);
 }
