43055ce8efb3d5e159a480d197c6d1424aebb8e7
Felixprogram <50058400+ImFelixprogram@users.noreply.github.com>
2021-04-29 15:23:30 +0200

    Fixing hackban.js

--- commands/moderation/hackban.js
+++ commands/moderation/hackban.js
@@ -14,29 +14,30 @@ module.exports = {
 }
 
 module.exports.execute = async(bot, msg, args, data) => {
-  let userID = args[0];
+	let userID = args[0];
   
-  if (!userID) return message.channel.send('Invalid User ID');
-  if (isNaN(userID)) return message.channel.send('Invalid User ID (can only contain numbers)');
-  if (userID === message.author.id) return message.reply('Why would you want to ban yourself?');
-  if (userID === client.user.id) return message.reply('Why would you ban me?');
-  if (userID === message.guild.ownerID) return message.reply('Silly you, trying to ban the Server Owner');
+	if (!userID) return message.channel.send('Invalid User ID');
+	if (isNaN(userID)) return message.channel.send('Invalid User ID (can only contain numbers)');
+	if (userID === message.author.id) return message.reply('Why would you want to ban yourself?');
+	if (userID === client.user.id) return message.reply('Why would you ban me?');
+	if (userID === message.guild.ownerID) return message.reply('Silly you, trying to ban the Server Owner');
   
-  const options = { };
-  reason = reason.length ? reason.join(' ') : null;
-  if (reason) options.reason = reason;
+	const options = { };
+	reason = reason.length ? reason.join(' ') : null;
+	if (reason) options.reason = reason;
   
-  client.users.fetch(userID).then(async user => {
-    await message.guild.members.ban(userID, {reason: reason});
+	client.users.fetch(userID).then(async user => {
+		await message.guild.members.ban(userID, {reason: reason});
     
-    if (guildDB.log_state === 'on') {
-      await messae.guild.channels.resolve(guildDB.logs).send(new Discord.MessageEmbed()
-                                                             .setAuthor(`<${userID}> got Hackbanned`)
-                                                             .setDescription(`${message.author.tag} hackbanned <${userID}> ${reason ? ` with the Reason ${reason}` : ""}!`)
-                                                             .setFooter('Banned at')
-                                                             .setTimestamp()
-                                                             .setColor('RED'))
-    }
+		if (guildDB.log_state === 'on') {
+		  await messae.guild.channels.resolve(guildDB.logs).send(new Discord.MessageEmbed()
+																 .setAuthor(`<${userID}> got Hackbanned`)
+																 .setDescription(`${message.author.tag} hackbanned <${userID}> ${reason ? ` with the Reason ${reason}` : ""}!`)
+																 .setFooter('Banned at')
+																 .setTimestamp()
+																 .setColor('RED'))
+		}
     
-    return message.reply(`<${userID}> got hackbanned.${reason ? ` With the reason of: ${reason}` : ""}`);
+		return message.reply(`<${userID}> got hackbanned.${reason ? ` With the reason of: ${reason}` : ""}`);
+	})
 }
